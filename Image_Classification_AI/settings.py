6db37098ccc20084bc2c7f034249535ca1aac61b
veski4a <veskoveskov2006@gmail.com>
2025-06-24 17:02:51 +0300

    feat: Configure STATIC_ROOT for production

--- Image_Classification_AI/settings.py
+++ Image_Classification_AI/settings.py
@@ -115,6 +115,7 @@ LOGIN_URL = '/login/'
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Only needed if you're storing static files outside of app folders
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # This includes the project/static/ directory
@@ -132,10 +133,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
